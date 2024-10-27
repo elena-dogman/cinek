ed2f2d7c99386923dd83a18f9dc9385ab2c90005
elena-dogman <dogman.choli@gmail.com>
2024-10-27 12:21:40 +0300

    feat: create api functions

--- src/lib/getMovies.ts
+++ src/lib/getMovies.ts
@@ -6,6 +6,8 @@ const fetcher = async (url: URL, cacheTime?: number) => {
   url.searchParams.set('sort_by', 'popularity.desc');
   url.searchParams.set('language', 'en-US');
   url.searchParams.set('page', '1');
+  url.searchParams.set('region', 'KR');
+  url.searchParams.set('with_original_language', 'ko');
 
   const options: RequestInit = {
     method: 'GET',
@@ -19,4 +21,60 @@ const fetcher = async (url: URL, cacheTime?: number) => {
   };
   const response = await fetch(url.toString(), options);
   const data = (await response.json()) as SearchResults;
+
+  return data;
+};
+
+export const getNowPlaying = async () => {
+  const url = new URL('https://api.themoviedb.org/3/movie/now_playing');
+  const data = await fetcher(url);
+  data.results;
+};
+export const getUpcomingMovies = async () => {
+  const url = new URL('https://api.themoviedb.org/3/movie/upcoming');
+  const data = await fetcher(url);
+  return data.results;
+};
+
+export const getTopRatedMovies = async () => {
+  const url = new URL('https://api.themoviedb.org/3/movie/top_rated');
+  const data = await fetcher(url);
+  return data.results;
+};
+
+export const getPopularMovies = async () => {
+  const url = new URL('https://api.themoviedb.org/3/movie/popular');
+  const data = await fetcher(url);
+  return data.results;
+};
+
+export const getDiscoverMovies = async (id?: string, keywords?: string) => {
+  const url = new URL('https://api.themoviedb.org/3/discover/movie');
+
+  keywords && url.searchParams.set('with_keywords', keywords);
+  id && url.searchParams.set('with_genres', id);
+
+  const data = await fetcher(url);
+  return data.results;
+};
+
+export const getSearchedMovies = async (term: string) => {
+  const url = new URL('https://api.themoviedb.org/3/search/movie');
+  url.searchParams.set('query', term);
+
+  const data = await fetcher(url);
+  return data.results;
+};
+
+export const getMovieVideos = async (id?: string) => {
+  const url = new URL(`https://api.themoviedb.org/3/movie/${id}/videos`);
+
+  const data = await fetcher(url);
+  return data.results;
+};
+export const getMovieDetails = async (id?: string) => {
+  const url = new URL(`https://api.themoviedb.org/3/movie/${id}`);
+
+  const data = await fetcher(url);
+  return data;
 };
