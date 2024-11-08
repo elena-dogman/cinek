e7eb87f3c94c760ae3cb099142899098a1829156
elena-dogman <dogman.choli@gmail.com>
2024-11-08 13:03:12 +0300

    feat: develop genre page

--- src/app/genre/[id]/page.tsx
+++ src/app/genre/[id]/page.tsx
@@ -1,5 +1,27 @@
-const GenrePage = () => {
-  return <div></div>;
+import MovieContainer from '~/components/MovieContainer';
+import { getDiscoverMovies } from '~/lib/getMovies';
+
+type GenrePageProps = {
+  params: {
+    id: string;
+  };
+  searchParams: {
+    genre: string;
+  };
+};
+
+const GenrePage = async ({ params: { id }, searchParams: { genre } }: GenrePageProps) => {
+  const movies = await getDiscoverMovies(id);
+  return (
+    <div className='msx-w-xl py-10'>
+      <h2 className='mx-auto mb-5 px-10 text-4xl font-bold'>Results for {genre}</h2>
+      <MovieContainer
+        movies={movies}
+        title='Genre'
+        isVertical={true}
+      />
+    </div>
+  );
 };
 
 export default GenrePage;
